44c837b15eb24910dec190bda98b60167c86c729
Jamie Garner <jamiegarner123@hotmail.co.uk>
2017-03-12 13:28:24 +0000

    Fix CORS enabling not being applied to request

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var index = require('./routes/recipes');
 var app = express();
 
-app.use('/', index);
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use('/', index);
 
 var port = process.env.PORT || '3001';
 
