77737c0bd1fc1c9483b8424b881af1eca71acf3d
Jamie Garner <jamiegarner123@hotmail.co.uk>
2017-03-12 12:36:51 +0000

    enable CORS

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var app = express();
 
 app.use('/', index);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var port = process.env.PORT || '3001';
 
 app.listen(port, function(error) {
